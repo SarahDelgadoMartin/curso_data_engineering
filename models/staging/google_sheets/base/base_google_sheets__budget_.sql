7fdd79d01477e93dbfbc9b046f06c6d85fc7d754
Sarah Delgado Martin <sarahdelgadomartin93@gmail.com>
2025-05-05 18:04:28 +0000

    23

--- models/staging/google_sheets/base/base_google_sheets__budget_.sql
+++ models/staging/google_sheets/base/base_google_sheets__budget_.sql
@@ -11,10 +11,11 @@ WITH src_budget AS (
 
 renamed_casted AS (
     SELECT
-        dbt_utils.generate_surrogate_key(_row) AS budget_id,
-        dbt_utils.generate_surrogate_key(product_id) AS product_id,
+        dbt_utils.generate_surrogate_key(['product_id', 'month']) AS budget_id,
+        CAST(product_id AS VARCHAR(256)) AS product_id,
         CAST(quantity AS INT) AS target_quantity,
         MONTH(month) AS month,
+        YEAR(month) AS year,
         CAST(CONVERT_TIMEZONE('Europe/Madrid', 'UTC', _fivetran_synced) AS DATE) AS date_load
     FROM src_budget
     )
