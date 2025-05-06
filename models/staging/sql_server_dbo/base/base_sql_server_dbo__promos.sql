ad45750600f450cae53328697eaed88e9d84478b
Sarah Delgado Martin <sarahdelgadomartin93@gmail.com>
2025-05-06 18:01:55 +0000

    martes tarde

--- models/staging/sql_server_dbo/base/base_sql_server_dbo__promos.sql
+++ models/staging/sql_server_dbo/base/base_sql_server_dbo__promos.sql
@@ -11,27 +11,29 @@ WITH src_promos AS (
 
 renamed_casted AS (
     SELECT
-        dbt_utils.generate_surrogate_key(promo_id) AS promo_id,
+        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} AS promo_id,
         CAST(promo_id AS VARCHAR(50)) AS desc_promo,
         CAST(discount AS INT) AS discount_units,
         CAST(CASE status
-            WHEN "inactive" THEN false
-            WHEN "active" THEN true
+            WHEN 'inactive' THEN false
+            WHEN 'active' THEN true
         END AS BOOLEAN) AS status,
-        CAST(CONVERT_TIMEZONE('Europe/Madrid', 'UTC', _fivetran_deleted) AS DATE) AS date_delete,
-        CAST(CONVERT_TIMEZONE('Europe/Madrid', 'UTC', _fivetran_synced) AS DATE) AS date_load
+        CAST(_fivetran_deleted AS BOOLEAN) AS is_delete,
+        CONVERT_TIMEZONE('UTC', CAST(_fivetran_synced AS TIMESTAMP_TZ)) AS date_load
     FROM src_promos
     ),
 
 new_row AS (
     SELECT
-       dbt_utils.generate_surrogate_key('no promo') AS promo_id
+       {{ dbt.hash(["no promo"]) }} AS promo_id,
        'no promo' AS desc_promo,
        0 AS discount_units,
        true AS status,
-       CAST(NULL AS DATE) AS date_delete,
-        CAST(CONVERT_TIMEZONE(SESSION_TIMEZONE, 'UTC', CURRENT_TIMESTAMP()) AS DATE) AS date_load
+       NULL AS date_delete,
+       CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP()) AS date_load
 
 )    
 
-SELECT * FROM renamed_casted UNION ALL new_row
+SELECT * FROM renamed_casted
+UNION ALL
+SELECT * FROM new_row
